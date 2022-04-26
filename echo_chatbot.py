1c06ca0fb0ec5cf4bb34656f7c851656044c3bfd
K.C <kmacallaghan@gmail.com>
2022-04-25 21:30:45 -0400

    Create echo_chatbot.py

---
+++ echo_chatbot.py
@@ -0,0 +1,7 @@
+#!python3 
+#Bot that repeats what is typed back to the user. 
+
+print("Welcome to Kcall174's first chatbot")
+print("I, ChatBot, will repeat what is said")
+stuff_to_echo = input("Say something... \n")
+print("You said: " + stuff_to_echo)
